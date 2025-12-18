54d4a71691705b8f4f125762209897cd9226d90e
Asmaa Youssef <asmaayoussef776@gmail.com>
2025-12-18 08:51:09 +0200

    Add footer module

---
+++ personal_portfolio/modules/footer.module/module.js
@@ -0,0 +1,7 @@
+// footer.js
+document.addEventListener("DOMContentLoaded", function() {
+  const yearSpan = document.getElementById("currentYear");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
